4b6215b530f40757636e5b6af9e38320910bed54
Chloe Rodriguez <chloerod2002@hotmail.com>
2023-02-27 22:55:13 -0500

    added grid and hover

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 const cards = [
-    {name:"Great Britian", src: "https://imgur.com/hPuhqv6.png"}
-    {name:"South Korea", src: "https://imgur.com/LtdREaJ.png"}
+    {name:"Great Britian", src: "https://imgur.com/hPuhqv6.png"},
+    {name:"South Korea", src: "../images/Flag_of_South_korea.png"}
     ]
-    
+
+const card1 = document.getElementById('card1')
+console.log(card1)
+
+
+document.getElementById("card1").src = cards[1].src
+
+
