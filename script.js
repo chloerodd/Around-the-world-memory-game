b8bf7377779ab8b3f555ccf060d1b377059e3d75
Chloe Rodriguez <chloerod2002@hotmail.com>
2023-03-02 17:10:42 -0500

    added images

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 const cards = [
-    {name:"Great Britian", src: "https://imgur.com/hPuhqv6.png"},
-    {name:"South Korea", src: "../images/Flag_of_South_korea.png"}
+    {name:"United Kingdom", src: "../images/Flag_of_the_United_Kingdom.png"},
+    {name:"South Korea", src: "../images/Flag_of_South_Korea.png"},
+    {name:"Mexico", src:"../images/Flag_of_Mexico_1917.png"},
+    {name:"Belgium", src:"../images/Flag_of_Belgium.png"},
+    {name:"Tanzania", src:"../images/Flag_of_Tanzania.png"},
+    {name:"Chile", src:"../images/Flags_of_Chile.png"},
+    {name:"India", src:"../images/Flag_of_India.png"},
+    {name:"Greenland", src:"../images/Flag_of_Greenland.png"}
     ]
 
-const card1 = document.getElementById('card1')
-console.log(card1)
+// const card1 = document.getElementById('card1')
 
 
-document.getElementById("card1").src = cards[1].src
+
+document.getElementById("card1" + "card14").src = cards[1].src
+document.getElementById("card3" + "card12").src = cards[2].src
 
 
