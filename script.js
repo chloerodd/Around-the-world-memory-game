ace02cdad084502827bb2e038664a59f89f3e8f1
Chloe Rodriguez <chloerod2002@hotmail.com>
2023-03-02 22:05:18 -0500

    added click

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const cards = [
-    {name:"United Kingdom", src: "../images/Flag_of_the_United_Kingdom.png"},
-    {name:"South Korea", src: "../images/Flag_of_South_Korea.png"},
+    {name:"United Kingdom", src:"../images/Flag_of_the_United_Kingdom.png"},
+    {name:"South Korea", src:"../images/Flag_of_South_Korea.png"},
     {name:"Mexico", src:"../images/Flag_of_Mexico_1917.png"},
     {name:"Belgium", src:"../images/Flag_of_Belgium.png"},
     {name:"Tanzania", src:"../images/Flag_of_Tanzania.png"},
@@ -10,10 +10,23 @@ const cards = [
     ]
 
 // const card1 = document.getElementById('card1')
+// const card2 = document.getElementById('card2')
 
+// document.getElementById("card2").src = cards[0].src
+// document.getElementById("card1").src = cards[1].src
+// document.getElementById("card14").src = cards[1].src
 
-
-document.getElementById("card1" + "card14").src = cards[1].src
-document.getElementById("card3" + "card12").src = cards[2].src
-
-
+const cardFlip = document.querySelectorAll('.card')
+// cardFlip.forEach(function(card) {
+//     card.addEventListener('click', function() {
+//         console.log('clicked') 
+//     })
+// })
+cardFlip.forEach(card => {
+    card.addEventListener('click', () => {
+        card.classList.add('flip')
+        setTimeout(() => {
+            
+        }, 500);
+    })
+})
